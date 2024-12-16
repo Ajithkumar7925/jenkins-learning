622100c2c44b31cc2373e289fa0e9197da34da8b
Ajithkumar7925 <ajiiikec007@gmail.com>
2024-12-16 11:59:52 +0530

    added provider

--- dev/main.tf
+++ dev/main.tf
@@ -2,12 +2,3 @@ module "ec2_module" {
   source = "../modules/ec2-module"
   inst_type = var.inst_type
 }
-
-resource "aws_cloudwatch_log_group" "jenkins-cw-log" {
-  name = "jenkins-cw-log"
-
-  tags = {
-    Environment = "production"
-    Application = "serviceA"
-  }
-}
